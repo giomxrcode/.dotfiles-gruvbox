433404dccb099d0af76de10dd61a54e414ac477c
giomxrcode <giomxr.code@gmail.com>
2024-10-02 22:30:56 -0400

    added: neovim config directory

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,22 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+-- LazyVim auto format
+vim.g.autoformat = true
+
+local opt = vim.opt
+
+opt.cursorline = true -- Enable/Disable highlighting of the current line
+opt.relativenumber = false -- Enable/Disable relative numbers
+opt.termguicolors = true -- Enable/Disable terminal true colors
+
+if vim.fn.has("nvim-0.10") == 1 then
+  opt.smoothscroll = true
+  opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
+  opt.foldmethod = "expr"
+  opt.foldtext = ""
+else
+  opt.foldmethod = "indent"
+  opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
+end
